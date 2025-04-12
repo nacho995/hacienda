502ad0e1d4e4bebd8a5361674dbf6c3a26782582
nacho995 <ignaciodalesio1995@gmail.com>
2025-04-12 09:19:39 +0200

    arreglos en funcionalidad de admin

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   experimental: {
     serverActions: {
       allowedOrigins: ['localhost:3000', 'localhost:3001', 'localhost:5000'],
