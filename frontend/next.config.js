998b2a56207e587fecdcd218adf75e35c114b71c
nacho995 <ignaciodalesio1995@gmail.com>
2025-04-12 09:03:43 +0200

    render

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   experimental: {
     serverActions: {
       allowedOrigins: ['localhost:3000', 'localhost:3001', 'localhost:5000'],
